68e9ce5922cf70c723eddd78616fa996bfa8c4bf
rauenah <seuemail@exemplo.com>
2026-02-04 13:44:02 -0300

    novos

---
+++ TABELA_IMPOSTO DE RENDA.sql
@@ -0,0 +1,9 @@
+select *from case_situacion_one_limpo;
+
+alter table case_situacion_one_limpo
+modify Renda_Anual decimal(10,2);
+
+alter table case_situacion_one_limpo
+modify Percentual_Imposto decimal(10,2);
+
+
